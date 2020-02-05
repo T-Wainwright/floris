a8b4f537f0d35f8f3bc3f12ed668d38dc7d11c50
T-Wainwright <tom.wainwright@bristol.ac.uk>
2020-02-05 03:57:51 -0600

    Improved optimization module

---
+++ cases/Hornsrev/horns_rev_points.py
@@ -0,0 +1,45 @@
+# Code to generate the coordinate points for Horns Rev 1
+
+# Import packages
+import numpy as np
+import matplotlib.pyplot as plt
+import floris.tools.optimization as optimization
+        
+# Define farm geometry
+n_col = 10
+n_row = 8
+
+# Rotor diameter- Used for turbine spacing
+D = 80
+
+# Skew angles for grid (Degrees)
+x_skew = 0
+y_skew = 7.2
+
+# Calculate farm geometery
+x_space = 7*D
+y_space = 7*D
+
+Farm1 = optimization.lay_opt(n_row,n_col,x_skew,y_skew,D,x_space,y_space)
+
+Farm1.generate_grid()
+
+Farm1.plot_farm()
+
+Farm1.write_out('hornsrev.pts')
+
+# Define perturbation vectors
+
+# Column shift directions
+y_col_shift = (0,0,0,D,0,0,2*D,0,0,0)
+x_col_shift = (0,0,0,D,0,-D,0,0,0,0)
+
+# Row shift directions
+y_row_shift = (0,0,D,0,3*D,0,0,0)
+x_row_shift = (0,-2*D,D,0,0,0,0,0)
+
+Farm1.modify(x_row_shift,x_col_shift,y_row_shift,y_col_shift)
+
+Farm1.plot_farm()
+
+
--- floris/tools/optimization.py
+++ floris/tools/optimization.py
@@ -11,6 +11,7 @@
 
 import numpy as np
 from scipy.optimize import minimize
+import matplotlib.pyplot as plt
 
 # import warnings
 # warnings.simplefilter('ignore', RuntimeWarning)
@@ -61,3 +62,58 @@ def optimize_yaw(fi, minimum_yaw_angle=0.0, maximum_yaw_angle=25.0):
     opt_yaw_angles = residual_plant.x
 
     return opt_yaw_angles
+
+class lay_opt:
+
+    def __init__(self, n_row, n_col, x_skew, y_skew, D, x_space, y_space):
+        # Initialise farm variables
+        self.n_row = n_row
+        self.n_col = n_col
+        self.x_skew = np.radians(x_skew)
+        self.y_skew = np.radians(y_skew)
+        self.D = D
+        self.x_space = x_space
+        self.y_space = y_space
+        self.n_turb = n_row*n_col
+
+        # Create array to store turbine positions
+        self.positions = np.zeros((self.n_turb,2))
+        # Create transformation matrix
+        self.T=[[np.cos(self.x_skew), -np.sin(self.y_skew)], [np.sin(self.x_skew), np.cos(self.y_skew)]]
+        
+
+    def generate_grid(self):
+        # Generate initial grid
+        X, Y = np.mgrid[0:self.n_col, 0:self.n_row]
+        X = X * self.x_space
+        Y = Y * self.y_space
+        positions = np.vstack([X.ravel(), Y.ravel()])
+
+        # Apply skew
+        positions2 = np.transpose(np.matmul(self.T,positions))
+
+        # Position farm to fully within the domain, and store
+        self.positions[:,0] = positions2[:,0] - np.min(positions2[:,0])
+        self.positions[:,1] = positions2[:,1] - np.min(positions2[:,1])
+
+        return()
+
+    def plot_farm(self):
+        plt.plot(self.positions[:,0],self.positions[:,1],'.')
+        plt.show()
+
+    def write_out(self,ftag):
+        with open(ftag, 'w') as f:
+            for point in self.positions:
+                f.write("%f, %f\n" % (point[0], point[1]))
+    
+    def modify(self,x_row_shift,x_col_shift,y_row_shift,y_col_shift):
+        ii = 0
+
+        for j in range(self.n_col):
+            for i in range(self.n_row):
+                # Create perturbation vector
+                delta = (x_col_shift[j]+x_row_shift[i],y_col_shift[j]+y_row_shift[i])
+                # Apply perturbation vector
+                self.positions[ii,:] = self.positions[ii,:] + np.matmul(self.T,delta)
+                ii=ii+1
